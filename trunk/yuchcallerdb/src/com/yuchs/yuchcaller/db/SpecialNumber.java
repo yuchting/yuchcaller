baf5e52a280451d9007a24d42ed74b2893adc4e4
YuchTing@gmail.com <YuchTing@gmail.com@fef83fb0-d262-87e4-276a-05a7f76cedab>
2012-11-08 15:37:51 +0000

    增加 解析特殊号码，排除重复条目的功能

--- trunk/yuchcallerdb/src/com/yuchs/yuchcaller/db/SpecialNumber.java
+++ trunk/yuchcallerdb/src/com/yuchs/yuchcaller/db/SpecialNumber.java
@@ -1,7 +1,14 @@
 package com.yuchs.yuchcaller.db;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.Vector;
 
 public class SpecialNumber extends BinSearchNumber implements Comparable<SpecialNumber>{
 	
@@ -39,5 +46,71 @@ public class SpecialNumber extends BinSearchNumber implements Comparable<Special
 		}
 		
 		return 0;
+	}
+	
+	
+	// get rid of the same number
+	public static void main(String[] _arg)throws Exception{
+		if(_arg.length < 2){
+			System.out.println("please input the file in order to get rid of same number");
+			return;
+		}
+		
+		Vector<SpecialNumber> t_list = new Vector<SpecialNumber>();
+		
+		BufferedReader in = new BufferedReader(new InputStreamReader(
+											new FileInputStream(_arg[1]),"UTF-8"));
+		try{
+			String line;
+			
+			topWhile:
+			while((line = in.readLine()) != null){
+				
+				String[] t_param = line.split("\t");
+				
+				if(t_param.length != 2){
+					System.err.println(line + " is error to split!");
+					continue;
+				}
+				
+				int t_number;
+				try{
+					t_number = Integer.parseInt(t_param[1]);
+				}catch(Exception e){
+					System.err.println(line + " number parse error:" + e.getMessage());
+					continue;
+				}				
+				
+				// search list
+				for(SpecialNumber sn : t_list){
+					if(sn.Compare(t_number) == 0){
+						continue topWhile;
+					}
+				}
+				
+				SpecialNumber t_sn = new SpecialNumber();
+				t_sn.m_number	= t_number;
+				t_sn.m_presents = t_param[0];
+				
+				t_list.add(t_sn);
+			
+			}
+		}finally{
+			in.close();
+		}
+		
+		final String t_outputFile = "spcialNumber_unique.txt";
+		
+		BufferedWriter os = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(t_outputFile)));
+		try{
+			for(SpecialNumber sn : t_list){
+				os.write(sn.m_presents + "\t" + sn.m_number);
+				os.write('\n');
+			}
+		}finally{
+			os.close();
+		}
+		
+		System.out.println("Write " + t_list.size() + " unique line to file " + t_outputFile);
 	}
 }
